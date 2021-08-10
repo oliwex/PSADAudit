b466608df48eab2a4d58f13a14e1062d11277108
Minio <oliwexminio@gmail.com>
2021-08-10 19:49:51 +0200

    Add ComputerFunction[NEED CHECKING]

--- skrypt.ps1
+++ skrypt.ps1
@@ -1,6 +1,4 @@
-﻿function Invoke-ADAudit
-{
-
+﻿
 #TODO:Create unified function for table
 #TODO:Create unified function for list
 ##########################################################################################
@@ -232,6 +230,87 @@ function Get-USERInformation {
     }
     $userOutput
 }
+
+#TEST
+function Get-ComputerInformation {
+
+
+    $computerData = Get-ADComputer -Filter * -Properties *
+    $computerOutput = foreach ($data in $userData) {
+    #AccountExpires,
+    [PSCustomObject] @{
+        'AccountExpirationDate' = $data.AccountExpirationDate
+        'AccountLockoutTime'    = $data.AccountLockoutTime
+        'AccountNotDelegated'   = $data.AccountNotDelegated
+        'AllowReversiblePasswordEncryption' = $data.AllowReversiblePasswordEncryption
+        'AuthenticationPolicy'   = $data.AuthenticationPolicy
+        'AuthenticationPolicySilo'  = $data.AuthenticationPolicySilo
+        'BadLogonCount' = $data.BadLogonCount
+        'CannotChangePassword'  = $data.CannotChangePassword
+        'CanonicalName' = $data.CanonicalName
+        'Certificates' = $data.Certificates
+        'CommonName' = $data.CommonName
+        'CodePage' = $data.codepage
+        #'CountryCode' = $data.CountryCode
+        'Description' = $data.Description
+        'DisplayName' = $data.DisplayName
+        'DistinguishedName' = $data.DistinguishedName
+        'DNSHostName' = $data.DNSHostName
+        'DoesNotRequirePreAuth' = $data.DoesNotRequirePreAuth
+        'Enabled' = $data.Enabled
+        'HomeDirRequired' = $data.HomeDirRequired
+        'HomePage' = $data.HomePage
+        'InstanceType' = $data.instanceType
+        'IP4' = $data.IPv4Address
+        'IP6' = $data.IPv6Address
+        'IsCriticalSystemObject' = $data.isCriticalSystemObject
+        'KerberosEncryptionType' = $data.KerberosEncryptionType
+        'LastBadPasswordAttempt' = $data.LastBadPasswordAttempt
+        'LastKnownParent' = $data.LastKnownParent
+        'LastLogonDate' = $data.LastLogonDate
+        #'LocalPolicyFlags' = $data.LocalPolicyFlags
+        'Location' = $data.Location
+        'LockedOut' = $data.LockedOut
+        'LogonCount' = $data.LogonCount
+        'ManagedBy' = $data.ManagedBy
+        'MemberOf' = $data.MemberOf
+        'Name' = $data.Name
+        'ObjectCategory' = $data.ObjectCategory
+        'ObjectClass' = $data.ObjectClass
+        'ObjectGUID' = $data.ObjectGUID
+        'OperatingSystem' = $data.OperatingSystem
+        'OperatingSystemHotfix' = $data.OperatingSystemHotfix
+        'OperatingSystemServicePack' = $data.OperatinSystemServicePack
+        'OperatingSystemVersion' = $data.OperatingSystemVersion
+        'PasswordExpired' = $data.PasswordExpired
+        'PasswordLastSet' = $data.PasswordLastSet
+        'PasswordNeverExpires' = $data.PasswordNeverExpires
+        'PasswordNotRequired' = $data.PasswordNotRequired
+        'PrimaryGroup' = $data.PrimaryGroup
+        'PrincipalsAllowedToDelegateToAccount' = $data.PrincipalsAllowedToDelegateToAccount
+        'ProtectedFromAccidentalDeletion' = $data.ProtectedFromAccidentalDeletion
+        'SamAccountName' = $data.SamAccountName
+        'SamAccountType' = $data.SamAccountType
+        'ServiceAccount' = $data.ServiceAccount
+        'ServicePrincipalName' = $data.ServicePrincipalName
+        'ServicePrincipalNames' = $data.ServicePrincipalNames
+        'SID' = $data.SID
+        'SIDHistory' = $data.SIDHistory
+        'TrustedForDelegation' = $data.TrustedForDelegation
+        'TrustedToAuthForDelegation' = $data.TrustedToAuthForDelegation
+        'UseDESKeyOnly' = $data.UseDESKeyOnly
+        'userAccountControl' = $data.userAccountControl
+        'UserCertificate' = $data.UserCertificate
+        'UserPrincipalName' = $data.UserPrincipalName
+        'USNChanged'    = $data.uSNChanged
+        'USNCreated'    = $data.uSNCreated
+        'WhenChanged'   = $data.whenChanged
+        'WhenCreated'   = $data.whenCreated
+        }
+    }
+$computerOutput 
+}
+
 function Get-GPOPolicy {
     Param(
         [Parameter(Mandatory = $true)]
@@ -713,6 +792,3 @@ foreach ($fgpp in $fgpps) {
 ##############################################################################################################
 Save-WordDocument $reportFile -Supress $true -Language "pl-PL" -Verbose #-OpenDocument
 Invoke-Item -Path $reportFilePath
-}
-
-Invoke-ADAudit
