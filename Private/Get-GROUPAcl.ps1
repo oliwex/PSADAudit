378fcca7ccf10e04a92ae292930cf6108c5c7692
Minio <oliwexminio@gmail.com>
2021-09-20 20:08:50 +0200

    Create Group Acl

---
+++ Private/Get-GROUPAcl.ps1
@@ -0,0 +1,24 @@
+﻿function Get-GROUPACL {
+    Param(
+        [Parameter(Mandatory = $true)]
+        [alias("GROUP_ACL", "GroupAccessControlList")]
+        [String] $groupPath
+    )
+
+    $path = "AD:\" + $groupDistinguishedName
+    $acls = (Get-Acl -Path $path).Access | Select-Object ActiveDirectoryRights,AccessControlType,IdentityReference,InheritanceType,InheritanceFlags,PropagationFlags
+    $info = (Get-ACL -Path $path | Select-Object Owner, Group, 'AreAccessRulesProtected', 'AreAuditRulesProtected', 'AreAccessRulesCanonical', 'AreAuditRulesCanonical')
+
+    [PSCustomObject] @{
+        'DN'                         = $groupDistinguishedName
+        'Owner'                      = $info.Owner
+        'Group'                      = $info.Group
+        'Are Access Rules Protected' = $info.'AreAccessRulesProtected'
+        'Are AuditRules Protected'   = $info.'AreAuditRulesProtected'
+        'Are Access Rules Canonical' = $info.'AreAccessRulesCanonical'
+        'Are Audit Rules Canonical'  = $info.'AreAuditRulesCanonical'
+        'ACLs'                       = $acls
+    }
+}
+
+ 
